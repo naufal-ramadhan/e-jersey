96ab36b4ec421c31ed6031bc5fcfbcc93b214687
naufal-ramadhan <naufalnauf04@gmail.com>
2024-09-09 14:48:00 +0700

    edit intial page

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('data-diri/', views.dataDiri, name='datadiri'),
+    path('home/', views.home, name='home'),
+    path('', views.dataDiri, name='datadiri'),
     path('club/', views.clubSelection, name='club'),
     path('national/', views.nationalSelection, name='national'),
     path('<str:type>/<slug:slug>', views.viewJersey, name='view'),
