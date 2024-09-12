6b2ff6f05b92aa6a78160079a75a46b411ae2f9b
naufal-ramadhan <naufalnauf04@gmail.com>
2024-09-12 15:58:33 +0700

    remove wrong urls and views

--- main/urls.py
+++ main/urls.py
@@ -4,9 +4,5 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
     path('', views.dataDiri, name='datadiri'),
-    path('club/', views.clubSelection, name='club'),
-    path('national/', views.nationalSelection, name='national'),
-    path('<str:type>/<slug:slug>', views.viewJersey, name='view'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,13 +2,6 @@ from django.shortcuts import render,get_object_or_404
 from .models import Product
 
 # Create your views here.
-def home(request):
-    context = {
-        "title" : "Home",
-    }
-
-    return render(request, "main.html", context)
-
 def dataDiri(request):
     context = {
         "nama" : "Muhammad Naufal Ramadhan",
@@ -17,29 +10,3 @@ def dataDiri(request):
     }
     return render(request, "dataDiri.html", context)
 
-def clubSelection(request):
-    jerseys = Product.objects.filter(type="club")
-    context = {
-        "title" : "Selection",
-        "type" : "Club Jersey",
-        "jerseys" : jerseys
-    }
-    return render(request, "selection.html", context)
-
-def nationalSelection(request):
-    jerseys = Product.objects.filter(type="national")
-    context = {
-        "title" : "Selection",
-        "type" : "National Jersey",
-        "jerseys" : jerseys
-    }
-    return render(request, "selection.html", context)
-
-def viewJersey(request, type, slug):
-    jersey = Product.objects.get(slug=slug)
-    context = {
-        "title" : "Jersey",
-        "type" : "Jersey",
-        "jersey" : jersey
-    }
-    return render(request, "view.html", context)
