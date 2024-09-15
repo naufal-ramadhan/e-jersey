46a6c9dc59ccb22b18bbabd7d3bc04242816fef7
naufal-ramadhan <naufalnauf04@gmail.com>
2024-09-15 13:20:45 +0700

    add id in product field

--- main/models.py
+++ main/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils.text import slugify
+import uuid
 
 # Create your models here.
 class Product(models.Model):
+     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
      name = models.CharField(max_length=255)
      price = models.IntegerField()
      description = models.TextField()
