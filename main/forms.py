fee599b8195220d01ace67564538bdd2874e0052
naufal-ramadhan <naufalnauf04@gmail.com>
2024-09-23 17:05:31 +0700

    add Template, View Function, and update Model for user (login related)

--- main/forms.py
+++ main/forms.py
@@ -2,6 +2,12 @@ from django.forms import ModelForm
 from main.models import Product
 
 class ProductForm(ModelForm):
+
+    JERSEY_TYPE = {
+        "club": "Club Jersey",
+        "nation": "Nation Jersey",
+    }
+
     class Meta:
         model = Product
-        fields = ["name", "price", "description", "type", "club"]
+        fields = ["name", "price", "description", "type"]
